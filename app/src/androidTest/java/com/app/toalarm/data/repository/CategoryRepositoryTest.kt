56559904e74261ad0b7723f5a54ba735ac5796b0
Arthur Galoyan <arthurgaloyan975@gmail.com>
2022-04-17 22:36:53 +0400

    commit

---
+++ app/src/androidTest/java/com/app/toalarm/data/repository/CategoryRepositoryTest.kt
@@ -0,0 +1,12 @@
+package com.app.toalarm.data.repository
+
+import org.junit.Assert.*
+
+import org.junit.Before
+
+class CategoryRepositoryTest {
+
+    @Before
+    fun setUp() {
+    }
+}
---
+++ app/src/androidTest/java/com/app/toalarm/viewmodel/AddTaskViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.app.toalarm.viewmodel
+
+import org.junit.Assert.*
+
+class AddTaskViewModelTest
